de8679ec42ee59e888a8ef1ddab8afc08ccb75af
Gilvan Jr <gilvan.junior@semprecomigo.com>
2021-04-22 15:48:46 -0300

    configurando paths no tsconfig

---
+++ src/core/entities/pessoa.ts
@@ -0,0 +1,3 @@
+export type Pessoa = {
+    name: string
+}
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express'
+import { Pessoa } from '@entities/pessoa'
 
 const server = express()
 
 server.get('/', (request: Request, response: Response) => {
+    const pessoa: Pessoa = {name: "Teste"}
+    console.log(pessoa)
     return response.status(200).json({ message: "Tudo ok!" })
 })
 
