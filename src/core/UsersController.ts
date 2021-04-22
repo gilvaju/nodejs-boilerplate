078e23577cd786ebb8996a7646de633456a16df1
Gilvan Jr <gilvan.junior@semprecomigo.com>
2021-04-22 16:07:29 -0300

    configurando o eslint

---
+++ src/core/UsersController.ts
@@ -0,0 +1,5 @@
+export class teste {
+  execute () {
+    console.log('teste')
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ import { Pessoa } from '@entities/pessoa'
 const server = express()
 
 server.get('/', (request: Request, response: Response) => {
-    const pessoa: Pessoa = {name: "Teste"}
-    console.log(pessoa)
-    return response.status(200).json({ message: "Tudo ok!" })
+  const pessoa: Pessoa = { name: 'Teste' }
+  console.log(pessoa)
+  return response.status(200).json({ message: 'Tudo ok!' })
 })
 
 server.listen(3333)
